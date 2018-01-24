3058d7fe78de33bf26c08b06cc25aacd479d1e03
Valeolivares <valentinaolivares85@gmail.com>
2018-01-24 00:48:55 -0300

    agregando funcion arreglada de cifrado

--- js/app.js
+++ js/app.js
@@ -1,21 +1,25 @@
 function cipher() {
   var text = document.getElementById('text').value;
-	if (text.length <= 0) {
-    alert("Favor ingresa un texto válido");
-  }else{
-    for (var i = 0; i < text.length; i++) {
-      var char = text[i];
-      var ascii = char.charCodeAt(i); //charCodeAt pasa a código ASCII
-      console.log(ascii);
-    }
+  var newMsje = [] ;
+  for (var i = 0; i < text.length; i++) {
+    var char = text[i];
+    var ascii = char.charCodeAt(0); 
+    var move = (((ascii - 65 + 33) % 26) + 65);
+    var cipherMsje = String.fromCharCode(move); // pasamos el ASCII a letras nuevamente
+    newMsje.push(cipherMsje);
+    var joinMsje = newMsje.join('');
+    var nContenedor = document.createElement('div');
+    var nCaja = document.getElementById('caja3');
+    var parrafo = document.createElement('p');
+    var nodoText = document.createTextNode(joinMsje);
+    parrafo.appendChild(nodoText);
+    nContenedor.appendChild(parrafo);
+    nCaja.appendChild(nContenedor);
   }
 }
 cipher();
 
 function decipher() {
-  var text = document.getElementById('text').value;
-  if (text.length <= 0) {
-    alert("Favor ingresa un texto válido");
-  }
+  
 }
 decipher();
